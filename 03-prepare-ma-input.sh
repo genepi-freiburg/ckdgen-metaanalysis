c83b0352a14f299735b17643d3ce59558269a637
Matthias Wuttke <matthias@wuttke.eu>
2017-07-03 14:32:27 +0200

    Skip existing files option

--- 03-prepare-ma-input.sh
+++ 03-prepare-ma-input.sh
@@ -1,9 +1,14 @@
 LOG=03-prepare-ma-input.log
 
+# 'secret' param that may be set to skip existing output files
+# this speeds up the process if you resume a previous run
+# WARNING: take care not to keep partial files
+SKIP_EXIST="$1"
+
 echo "LQ" | tee $LOG
 
-/shared/metaanalysis/scripts/prepare-ma-input.sh input-files-with-path.txt 0 0.3 1 100 10 1 input_LQ | tee -a $LOG
+/shared/metaanalysis/scripts/prepare-ma-input.sh input-files-with-path.txt 0 0.3 1 100 10 1 input_LQ $SKIP_EXIST | tee -a $LOG
 
 echo "HQ" | tee -a $LOG
 
-/shared/metaanalysis/scripts/prepare-ma-input.sh input-files-with-path.txt 0.05 0.6 10 100 10 1 input_HQ | tee -a $LOG
+/shared/metaanalysis/scripts/prepare-ma-input.sh input-files-with-path.txt 0.05 0.6 10 100 10 1 input_HQ $SKIP_EXIST | tee -a $LOG
